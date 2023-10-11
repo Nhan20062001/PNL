5f1ad6d17e311b1918f4c00d827497cb7793e63d
xuandiem2709 <xdiem2709@gmail.com>
2023-10-11 17:47:03 +0700

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,46 +2,23 @@
 
 import React from 'react';
 import { MainClient } from '@/layouts/MainClient';
-import styles from './page.module.scss';
-import { Image } from 'antd';
-import ButtonSample from '@/components/Button/button';
-import SendIcon from '@/components/icons/send.icons';
 import AboutUsCardComponent from '@/components/AboutUsCard/page';
+import WhatWeDoCard from '@/components/WhatWeDoCard/page';
+import OurCustomers from '@/components/OurCustomers/page';
+import Intro from '@/components/Intro/page';
 
 export default function Home() {
   return (
     <MainClient>
-      <div className={styles['container']}>
-        <div className={styles['main']}>
-          
-          <div className={styles['content-left']}>
-            <h3>Cung cấp giải pháp chuyển đổi số</h3>
-            <div className={styles['content-detail']}>
-              <h6>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book
-              </h6>
-            </div>
-            <ButtonSample>
-              <SendIcon />
-              <p>Contact to work</p>
-            </ButtonSample>
-          </div>
-          {/* end left content */}
-
-          <div className={styles['content-right']}>
-            <Image
-              className={styles['content-image-right']}
-              src='/images/header twiscode 1.png'
-              alt=''
-              width={656}
-              height={525}
-            />
-          </div>
-        </div>
-      </div>
-      {/* end contact */}
+      
+      <Intro />
 
       <AboutUsCardComponent/>
+
+      <WhatWeDoCard />
+
+      <OurCustomers />
+      
     </MainClient>
   );
 }
