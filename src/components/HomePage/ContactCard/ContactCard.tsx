ab4b8ed05d8ffe5ca3df793b24d114cd379be4b2
SneakyTurtle <le741996cf@gmail.com>
2023-10-23 14:23:59 +0700

    fix-button-contact

--- src/components/HomePage/ContactCard/ContactCard.tsx
+++ src/components/HomePage/ContactCard/ContactCard.tsx
@@ -12,12 +12,12 @@ const ContactCardComponent = () => {
     <div className={styles['contact-card']}>
       <div className={styles['contact-container']}>
         <Image
-          className={styles['contact-container--image']}
+          className={styles['contact-container-image']}
           src="https://s3-alpha-sig.figma.com/img/69af/ef14/ec34ddc5ab531a19e86c9704fbeab279?Expires=1698019200&Signature=OC9m6AJYt~uXxRgDUOJ-2R6p6q5mDkWn2IYyWGwh87SctWwaeYJJAoF0~qYZaDVpXtIAFUZ3YBiZjawHVReKc~0c1A4AAKtJTE7Sw6~k6voMlSA8e2N9fZg~PWXjQDf0321WTWoUk6gMWcG0WjFmQ1n3vwZcpAhkRQZrHuIT7tpZQrI0QbINZDvdCda-MGLA68-t0vqMRJ5w16wht-o9THKHg9ZOAHEAaoCdSzbYLDcoekD7vb6BPZg9Vb002JXSBJlTXNSoDZ4vtQQXsixVIrEGQqvGy3uRiAJv3oO3HoFCQ-TrMU6vzKV~q3Rk8OnhvOOsGJXj5b2DfqGwCETKpQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
           alt=""
           preview={false}
         />
-        <div className={styles['contact-container--opacity-layer']}></div>
+        <div className={styles['contact-container-opacity-layer']}></div>
 
         <div className={styles['contact-card-content']}>
           <div className="container">
